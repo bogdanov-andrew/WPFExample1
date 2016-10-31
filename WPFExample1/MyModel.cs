4b2d3d6103f87e67cc03859bf144e86976745b25
Bogdanov Andrew <bogdanov.andrew.i@gmail.com>
2016-10-31 19:11:49 +0300

    Examples

---
+++ WPFExample1/MyModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace WPFExample1
+{
+    public class MyModel
+    {
+        public MyModel()
+        {
+            InitPersonList();
+        }
+
+        public List<Person> PersonList { get; set; }
+
+        private void InitPersonList()
+        {
+            PersonList = new List<Person>();
+            PersonList.Add(new Person(){FirstName = "code f1", LastName = "code l1", Tel = "code t1"});
+            PersonList.Add(new Person(){FirstName = "code f2", LastName = "code l2", Tel = "code t2"});
+            PersonList.Add(new Person(){FirstName = "code f3", LastName = "code l3", Tel = "code t3"});
+        }
+    }
+}
---
+++ WPFExample1/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WPFExample1
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Tel { get; set; }
+    }
+}
