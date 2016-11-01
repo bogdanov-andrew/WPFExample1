0632e6944bed6f6916a82ee5e088997a87b849b6
Bogdanov Andrew <bogdanov.andrew.i@gmail.com>
2016-11-01 16:46:13 +0300

    added commands example

---
+++ WPFExample1/DelegateCommand.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFExample1
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+        private readonly Func<bool> _canExecuteFunc;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public DelegateCommand(Action action, Func<bool> canExecuteFunc)
+            : this(action)
+        {
+            _canExecuteFunc = canExecuteFunc;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteFunc != null)
+            {
+                return _canExecuteFunc();
+            }
+
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+
+    public class DelegateCommand<T> : ICommand where T : class
+    {
+        private readonly Action<T> _action;
+        private readonly Func<bool> _canExecuteFunc;
+
+        public DelegateCommand(Action<T> action)
+        {
+            _action = action;
+        }
+
+        public DelegateCommand(Action<T> action, Func<bool> canExecuteFunc)
+            : this(action)
+        {
+            _canExecuteFunc = canExecuteFunc;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter as T);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteFunc != null)
+            {
+                return _canExecuteFunc();
+            }
+
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
--- WPFExample1/MyModel.cs
+++ WPFExample1/MyModel.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace WPFExample1
 {
-    public class MyModel
+    public class MyModel : INotifyPropertyChanged 
     {
+        private string _exampleText = "test text";
+
         public MyModel()
         {
             InitPersonList();
+            TestCommand = new DelegateCommand(TestAction, CanExecuteFunc);
+        }
+
+        private bool CanExecuteFunc()
+        {
+            //return true;
+            return !_textButtonAlreadyPressed;
+        }
+
+
+        private bool _textButtonAlreadyPressed = false;
+        private void TestAction()
+        {
+            ExampleText = DateTime.Now.ToLongTimeString();
+            _textButtonAlreadyPressed = true;
+        }
+
+        public string ExampleText
+        {
+            get { return _exampleText; }
+            set
+            {
+                _exampleText = value;
+                OnPropertyChanged();
+            }
         }
 
         public List<Person> PersonList { get; set; }
+
+        public ICommand TestCommand { get; set; }
 
         private void InitPersonList()
         {
@@ -17,6 +50,14 @@ namespace WPFExample1
             PersonList.Add(new Person(){FirstName = "code f1", LastName = "code l1", Tel = "code t1"});
             PersonList.Add(new Person(){FirstName = "code f2", LastName = "code l2", Tel = "code t2"});
             PersonList.Add(new Person(){FirstName = "code f3", LastName = "code l3", Tel = "code t3"});
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
